3739fc57acc2762890367d984b212619932fc148
Paul Rodwell <paul.rodwell@btinternet.com>
2026-04-29 09:55:21 +0100

    address audit issues,
    remove watch and update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,28 @@
 module.exports = function (grunt) {
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-git-authors');
+  grunt.loadNpmTasks("grunt-contrib-coffee");
+  grunt.loadNpmTasks("grunt-git-authors");
 
   grunt.initConfig({
-
     authors: {
       prior: [
         "Ward Cunningham <ward@c2.com>",
-        "Nick Niemeir <nick.niemeir@gmail.com>"
-      ]
+        "Nick Niemeir <nick.niemeir@gmail.com>",
+      ],
     },
 
     coffee: {
       client: {
         expand: true,
         options: {
-          transpile: { presets: ['@babel/preset-env'] },
-          sourceMap: true
+          transpile: { presets: ["@babel/preset-env"] },
+          sourceMap: true,
         },
-        src: ['client/*.coffee'],
-        ext: '.js'
-      }
+        src: ["client/*.coffee"],
+        ext: ".js",
+      },
     },
-
-    watch: {
-      all: {
-        files: ['client/*.coffee'],
-        tasks: ['coffee']
-      }
-    }
   });
 
-  grunt.registerTask('build', ['coffee']);
-  grunt.registerTask('default', ['build']);
-
+  grunt.registerTask("build", ["coffee"]);
+  grunt.registerTask("default", ["build"]);
 };
